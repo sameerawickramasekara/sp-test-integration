a0c0f71c61783f998e4d767db4bd582aca3ec6c5
vasanth889 <vasanth889@gmail.com>
2018-09-10 11:35:18 +0530

    changes in runscenario.sh

--- integration-tests/run-scenario.sh
+++ integration-tests/run-scenario.sh
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#set -e
-#set -o xtrace
+set -e
+set -o xtrace
 
 DIR=$2
 FILE1=${DIR}/infrastructure.properties
